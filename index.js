3f368ce7e5231955dbfb3f56176b0fbcc1d0eaf4
Jeffrey Penta <82849158+JeffreyPenta@users.noreply.github.com>
2021-11-07 11:17:03 -0500

    Add files via upload
    Dragon Dojo

---
+++ index.js
@@ -0,0 +1,12 @@
+var clicks = 0;
+
+
+    function hello() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
+var clicks2 = 0
+    function hello2() {
+        clicks2 += 1;
+        document.getElementById("clicks2").innerHTML = clicks2;
+    };
